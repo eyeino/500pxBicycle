6cadca6a011d9f599e4d7e59097420049f5e3148
ian <macfarlane@temple.edu>
2016-09-06 16:17:31 -0400

    chore: Add DeveloperParameters.swift to repo

---
+++ 500pxBicycle/DeveloperParameters.swift
@@ -0,0 +1,13 @@
+//
+//  DeveloperParameters.swift
+//  500pxBicycle
+//
+//  Created by Ian MacFarlane on 9/5/16.
+//  Copyright © 2016 Ian MacFarlane. All rights reserved.
+//
+
+import Foundation
+
+struct DeveloperParameters {
+    static let consumerKey = "INSERT CONSUMERKEY HERE"
+}
